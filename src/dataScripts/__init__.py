92a3cd936b512ba79e487f29fcca651dd36d8451
giovannipinna96 <giovanni.pinna.17l96@gmail.com>
2024-06-03 14:43:52 +0000

    add allowed DB

--- src/dataScripts/__init__.py
+++ src/dataScripts/__init__.py
@@ -4,3 +4,19 @@ SQL_KEYWORDS = [
     "RIGHT", "FULL", "OUTER", "GROUP", "BY", "ORDER", "HAVING", "UNION", "DISTINCT",
     "LIMIT", "OFFSET"
 ] 
+
+ALL_DB: dict[str, str] = {
+    'california schools': "california_schools",
+    'card games': "card_games",
+    'codebase community': "codebase_community",
+    'debit card specializing': "debit_card_specializing",
+    'european football': "european_football_2",
+    'financial': "financial",
+    'formula 1': "formuala_1",
+    'student club': "student_club",
+    'superhero': "superhero",
+    'thrombosis prediction': "thrombosis_prediction",
+    'toxicology': "toxicology",     
+}
+
+db_list = sorted([key for key in ALL_DB])
