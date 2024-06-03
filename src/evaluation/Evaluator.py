1444966c66cb7fc2ab06f9cc18b2a87e040e3eff
giovannipinna96 <giovanni.pinna.17l96@gmail.com>
2024-06-03 14:45:32 +0000

    refactor Evaluator and integrated VES in tableEvaluator

--- src/evaluation/Evaluator.py
+++ src/evaluation/Evaluator.py
@@ -1,26 +1,16 @@
 from abc import ABC, abstractmethod
 from typing import List
+from databaseFacilitator import DatabaseManager
+from math import sqrt
 
-class Evaluator(ABC):
-    
+class Evaluator(ABC):    
     def __init__(self) -> None:
         print("Init Evaluator")
         ...
     @abstractmethod
-    def evaluate(self, sqls: List[str] | str) -> List[float] | float:
+    def evaluate(self, sql_gold: str, sql_predicted: str, db_manager: DatabaseManager) -> List[float] | float:
         ...
-        
-class vesEvaluator(Evaluator):
-    def __init__(self) -> None:
-        super().__init__()
-        self.__name = "ves evaluator"
-        
-    @property
-    def name(self) -> str:
-        return self.__name
-    
-    def evaluate(self, sqls: List[str] | str) -> List[float] | float:
-        pass
+
     
 class sqlQueryEvaluator(Evaluator):
     def __init__(self) -> None:
@@ -31,7 +21,7 @@ class sqlQueryEvaluator(Evaluator):
     def name(self) -> str:
         return self.__name
     
-    def evaluate(self, sqls: List[str] | str) -> List[float] | float:
+    def evaluate(self, sql_gold: str, sql_predicted: str, db_manager: DatabaseManager) -> List[float] | float:
         pass
     
 class tableEvaluator(Evaluator):
@@ -43,6 +33,33 @@ class tableEvaluator(Evaluator):
     def name(self) -> str:
         return self.__name
     
-    def evaluate(self, sqls: List[str] | str) -> List[float] | float:
+    def evaluate(self, sql_gold: str, sql_predicted: str, db_manager: DatabaseManager) -> List[float] | float:
+        result_query_execution_gold, execution_time_gold = db_manager.query_executor(sql_gold)
+        result_query_execution_gen, execution_time_gen = db_manager.query_executor(sql_predicted)
         pass
+    
+    def table_evaluation(self, result_query_execution_gold, result_query_execution_gen) -> float:
+        # ! mettere parte di De Lorenzo
+        pass
+    
+    def is_equal(self, result_query_execution_gold, result_query_execution_gen) -> bool:
+        # ! qui inserire come le tabelle sono uguali o no
+        pass
+    
+    def ves_evaluation(self, result_query_execution_gold, result_query_execution_gen, execution_time_gold, execution_time_gen) -> float | int:
+        if self.is_equal(result_query_execution_gold, result_query_execution_gen):
+            return sqrt(execution_time_gold / execution_time_gen)
+        else: 
+            return 0
+        
+    def mean_ves_evaluation(self, result_query_execution_gold, result_query_execution_gen, execution_time_gold, execution_time_gen) -> float | int:
+        print("VES evaluation...")
+        if len(result_query_execution_gold) == len(result_query_execution_gen) == len(execution_time_gold) == len(execution_time_gen):
+            sum = 0
+            for i in range(len(execution_time_gold)):
+                sum += self.ves_evaluation(result_query_execution_gold[i], result_query_execution_gen[i], execution_time_gold[i], execution_time_gen[i])
+            return sum / len(execution_time_gold)
+        else:
+            raise ValueError("The lengths of the lists are not equal.")
+        
     
